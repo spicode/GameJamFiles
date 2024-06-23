a79a4a70c281ad18371d7a04cd40193d24de65d0
beans2011 <105490498+beans2011@users.noreply.github.com>
2024-06-23 12:45:27 +0300

    shalev pls help me with the controlls of the lasagzna ais th player with is cald enemy
    hepl

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,28 +1,47 @@
 extends CharacterBody2D
 
+# Player properties
+var speed = 200.0
+var jump_force = -150  # Negative value for upward force
+var gravity = 800.0
 
-const SPEED = 200
-const JUMP_VELOCITY = -300.0
+# State
+var is_on_ground = false
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
+func _ready():
+	pass
 
 func _physics_process(delta):
-	# Add the gravity.
+	handle_input(delta)
+
+	# Apply gravity
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
 	move_and_slide()
+
+func handle_input(delta):
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+
+	velocity.x = input_vector.x * speed
+
+	if is_on_floor() and Input.is_action_pressed("ui_up"):
+		jump()
+
+	# Debug prints
+	print("Velocity:", velocity)
+	print("Is on ground:", is_on_floor())
+
+func jump():
+	if is_on_floor():
+		velocity.y = jump_force 
+
+func _on_Foot_area_entered(area):
+	is_on_ground = true
+
+func _on_Foot_area_exited(area):
+	is_on_ground = false
+
+
+
