5d8e62d0a74be664d67285db05abdafe332f7932
spicode <126559883+spicode@users.noreply.github.com>
2024-06-23 15:45:09 +0300

    hands can hold things and move!!!!1!!!

--- scripts/drag_and_drop.gd
+++ scripts/drag_and_drop.gd
@@ -1,9 +1,8 @@
 extends Sprite2D
-var is_dragging = false #state management
 var mouse_offset #center mouse on click
 var delay = 3
 func _physics_process(delta):
-	if is_dragging == true:
+	if Global.is_dragging == true:
 		var tween = get_tree().create_tween()
 		tween.tween_property(self, "position", get_global_mouse_position()-mouse_offset, delay * delta)
 func _input(event):
@@ -11,7 +10,7 @@ func _input(event):
 		if event.pressed:
 			if get_rect().has_point(to_local(event.position)):
 				print('clicked on sprite')
-				is_dragging = true
+				Global.is_dragging = true
 				mouse_offset = get_global_mouse_position()-global_position
 		else:
-			is_dragging = false
+			Global.is_dragging = false
---
+++ scripts/hands.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+var mouse_position = null
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	
+	mouse_position = get_global_mouse_position()
+	global_position = mouse_position
+	if Global.is_dragging:
+		$Sprite2D.texture = load("res://art/HANDS 1 HOLDING.png")
+	else:
+		$Sprite2D.texture = load("res://art/HANDS 2 DEFAULT.png")
