291249eb95a7a7265e377c880e43eb094a6f67ab
spicode <126559883+spicode@users.noreply.github.com>
2024-06-25 15:30:30 +0300

    Revert "idc"
    This reverts commit 8c3f0b11b8bc22d7e722ea3bb938a288ac4b0fb1.

--- objects(like_player)/stayinplace.gd
+++ objects(like_player)/stayinplace.gd
@@ -4,7 +4,7 @@ extends Sprite2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	add_to_group("screen") # Replace with function body.
-	
+	$AnimatedSprite.frame = 0
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
