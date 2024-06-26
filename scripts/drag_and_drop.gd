eef7fb1e9688e164419c71ae4224f8d2a96ebfb0
spicode <126559883+spicode@users.noreply.github.com>
2024-06-26 12:28:59 +0300

    gafafafhdh

--- scripts/drag_and_drop.gd
+++ scripts/drag_and_drop.gd
@@ -2,6 +2,8 @@ extends Sprite2D
 var is_dragging = false #state management
 var mouse_offset   #center mouse on click
 var delay = 10
+var drop_spots
+
 func _ready():
 	add_to_group("foods")
 func _physics_process(delta):
@@ -23,6 +25,20 @@ func _input(event):
 		else:
 			is_dragging = false
 			Global.is_dragging = false
+	elif event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		if event.pressed:
+			if get_rect().has_point(to_local(event.position)):
+				print('clicked on sprite')
+				Global.is_Mixing = true
+				mouse_offset = get_global_mouse_position()-global_position
+		else:
+			
+			for drop_spot in drop_spots:
+				if drop_spot.has_overlapping_areas() and drop_spot.get_overlapping_areas().has(self.get_node("Area2D")):
+					var snap_position = drop_spot.position
+					var tween = get_tree().create_tween()
+					tween.tween_property(self, "position", snap_position, delay)
+
 
 
 func _on_area_2d_enterd(area):
--- scripts/sauce_ball.gd
+++ scripts/sauce_ball.gd
@@ -1,13 +1,49 @@
 extends AnimatedSprite2D
 
+var mouse_offset = Vector2(0,0) #center mouse on click
+var delay = .2
+var drop_spots
 
+func _ready():
+	$".".frame = 0
+
+func get_bounding_rect() -> Rect2:
+	var current_frame: int = get_frame()
+	var current_animation: String = animation
+	var sprite_frames: SpriteFrames
+
+	if sprite_frames and sprite_frames.has_animation(current_animation):
+		var texture: Texture2D = sprite_frames.get_frame(current_animation, current_frame)
+
+		if texture:
+			var size: Vector2 = texture.get_size()
+			var offset: Vector2 = Vector2.ZERO
+
+			if flip_h:
+				offset.x = -size.x
+			if flip_v:
+				offset.y = -size.y
+				return Rect2(offset * scale, size * scale)
+	return Rect2()
 
 func _input(event):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_RIGHT:
 		if event.pressed:
-			var sprite_rect = Rect2(Vector2.ZERO, $".".sprite_frames.get_size())
-			if sprite_rect.has_point(to_local(event.position)):
+			if Rect2().has_point(to_local(event.position)):
 				print(Global.is_Mixing)
 				Global.is_Mixing = true
+			else:
+				Global.is_Mixing = false
+				mouse_offset = get_global_mouse_position()-position
 		else:
 			Global.is_Mixing = false
+			for drop_spot in drop_spots:
+				if drop_spot.has_overlapping_areas() and drop_spot.get_overlapping_areas().has(self.get_node("Area2D")):
+					var snap_position = drop_spot.position
+					var tween = get_tree().create_tween()
+					tween.tween_property(self, "position", snap_position, delay)
+
+func is_point_in_sprite(point:Vector2,sprite) -> bool:
+	var texture = sprite.texture
+	var rect = Rect2(Vector2.ZERO, texture.get_size())
+	return rect.has_point(point)
