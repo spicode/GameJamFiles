1988d8caa4ac2dffd72618d057d3638e6316a484
spicode <126559883+spicode@users.noreply.github.com>
2024-06-26 08:39:36 +0300

    now it works

--- sauce_ball.gd
+++ sauce_ball.gd
@@ -4,24 +4,36 @@ var mouse_offset #center mouse on click
 var delay = .2
 var drop_spots
 
-func _ready():
-	drop_spots = get_tree().get_nodes_in_group("drop_spot_group")
-	print(drop_spots)
-func _physics_process(delta):
-	if Global.is_dragging == true:
-		var tween = get_tree().create_tween()
-		tween.tween_property(self, "position", get_global_mouse_position()-mouse_offset, delay * delta)
+func get_bounding_rect() -> Rect2:
+	var current_frame: int = get_frame()
+	var current_animation: String = animation
+	var sprite_frames: SpriteFrames
+
+	if sprite_frames and sprite_frames.has_animation(current_animation):
+		var texture: Texture2D = sprite_frames.get_frame(current_animation, current_frame)
+
+		if texture:
+			var size: Vector2 = texture.get_size()
+			var offset: Vector2 = Vector2.ZERO
+
+			if flip_h:
+				offset.x = -size.x
+			if flip_v:
+				offset.y = -size.y
+				return Rect2(offset * scale, size * scale)
+	return Rect2()
+
 func _input(event):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_RIGHT:
 		if event.pressed:
-			if get_rect().has_point(to_local(event.position)):
+			if Rect2().has_point(to_local(event.position)):
 				print(Global.is_Mixing)
 				Global.is_Mixing = true
 			else:
 				Global.is_Mixing = false
 	elif event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
 		if event.pressed:
-			if get_rect().has_point(to_local(event.position)):
+			if Rect2().has_point(to_local(event.position)):
 				print('clicked on sprite')
 				Global.is_dragging = true
 				mouse_offset = get_global_mouse_position()-global_position
@@ -32,4 +44,5 @@ func _input(event):
 					var snap_position = drop_spot.position
 					var tween = get_tree().create_tween()
 					tween.tween_property(self, "position", snap_position, delay)
+
 
