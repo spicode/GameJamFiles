a43abcb17bbefa53ffcf4ef65fd3d59a68c3ca0a
spicode <126559883+spicode@users.noreply.github.com>
2024-06-27 15:50:30 +0300

    drop spots

--- scripts/drag_and_drop.gd
+++ scripts/drag_and_drop.gd
@@ -1,18 +1,21 @@
 extends Sprite2D
 var is_dragging = false #state management
 var mouse_offset   #center mouse on click
-var delay = 10
+var delay_dragging = 5
+var delay_drop = 0.5
 var drop_spots
 
 func _ready():
 	add_to_group("foods")
+	drop_spots = get_tree().get_nodes_in_group("drop_spot_group")
+	
 func _physics_process(delta):
 	if Global.is_out_of_bounds:
 		position = Vector2(0,0)
 		Global.is_out_of_bounds = false
 	if is_dragging:
 			var tween = get_tree().create_tween()
-			tween.tween_property(self, "position",( get_global_mouse_position() - mouse_offset), delay * delta)
+			tween.tween_property(self, "position",( get_global_mouse_position() - mouse_offset), delay_dragging * delta)
 
 func _input(event):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
@@ -25,19 +28,13 @@ func _input(event):
 		else:
 			is_dragging = false
 			Global.is_dragging = false
-	elif event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
-		if event.pressed:
-			if get_rect().has_point(to_local(event.position)):
-				print('clicked on sprite')
-				Global.is_Mixing = true
-				mouse_offset = get_global_mouse_position()-position
-		else:
-			
+			print(drop_spots)
 			for drop_spot in drop_spots:
 				if drop_spot.has_overlapping_areas() and drop_spot.get_overlapping_areas().has(self.get_node("Area2D")):
 					var snap_position = drop_spot.position
 					var tween = get_tree().create_tween()
-					tween.tween_property(self, "position", snap_position, delay)
+					tween.tween_property(self, "position", snap_position, delay_drop)
+					print(drop_spot)
 
 
 
