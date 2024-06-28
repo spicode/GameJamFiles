09c538456b67bc2202e787a2fe70865f723db9ae
spicode <126559883+spicode@users.noreply.github.com>
2024-06-28 09:35:42 +0300

    level 1

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,8 +1,8 @@
 extends CharacterBody2D
 
 # Player properties
-var speed = 200.0
-var jump_force = -300.0  # Negative value for upward force
+var speed = 240.0
+var jump_force = -350.0  # Negative value for upward force
 var gravity = 800.0
 @onready var _animated_sprite = $AnimatedSprite2D
 # State
