4a8f6381213c46c6eaf0f1a99d6968513ac065dd
spicode <126559883+spicode@users.noreply.github.com>
2024-06-30 11:46:31 +0300

    respies

--- objects(like_player)/bowls.gd
+++ objects(like_player)/bowls.gd
@@ -6,8 +6,8 @@ var Respie_making = 1
 var Respie_lvl = 0
 var making_sauce = false
 var mouse_offset = Vector2(0,0) 
-var respie_layers = ["flour","egg","water"]
-var respie_sauce = ["oliveOil",]
+var respie_layers = ["flour","egg","water",""]
+var respie_sauce = ["condecsedTomato","oliveOil","parlsy","garlic","onion","tomato","salt",""]
 @onready var layers_bowl = $LayersBowl
 @onready var sauce_bowl = $SauceBowl
 
@@ -19,6 +19,7 @@ func _ready():
 	$SauceBowl.visible = false
 	$LayersBowl.frame = 0
 	$"../ingredints/lasagna layers".visible = false
+	$"../ingredints/SauceBowlI".visible = false
 	Respie_making = 1
 	if is_instance_valid(layers_bowl):
 		print("LayersBowl node found!")
@@ -26,30 +27,31 @@ func _ready():
 		print("LayersBowl node not found!")
 	await $LayersBowl.animation_finished
 func _process(delta):
-
+	
+	if Respie_making == 2:
+		$SauceBowl.visible = true
 	if is_instance_valid(layers_bowl) and layers_bowl.frame == 3:
 		if Global.is_Mixing:
 			layers_bowl.frame = 4
-			print(Global.is_Mixing)
-			get_tree().create_timer(1.0).timeout
-			Respie_making += 1
 			
+			get_tree().create_timer(5.0).timeout
+			Respie_making += 1
+			Respie_lvl = 0
 			layers_bowl.queue_free()
 			$"../ingredints/lasagna layers".visible = true
-	if is_instance_valid(sauce_bowl) and sauce_bowl.frame == 3:
+			
+	if is_instance_valid(sauce_bowl)and sauce_bowl.frame == 7:
 		if Global.is_Mixing:
-			sauce_bowl.frame = 4
-			print(Global.is_Mixing)
-			get_tree().create_timer(1.0).timeout
+			sauce_bowl.frame = 8
+			get_tree().create_timer(4).timeout
 			Respie_making += 1
-			
-			
-			
+			$SauceBowl.queue_free()
+			$"../ingredints/SauceBowlI".visible = true
 func _input(event):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_RIGHT:
 		if event.pressed:
 			if get_viewport_rect().has_point(to_local(event.position)):
-				print(Global.is_Mixing)
+				
 				Global.is_Mixing = true
 			else:
 				Global.is_Mixing = false
@@ -73,28 +75,26 @@ func _input(event):
 
 func _on_area_layer_area_entered(area):
 	var body = area.get_parent()
-	print(body.name)
-	print(Respie_lvl)
+	
+	
 	if Respie_making == 1:
 		if area.get_parent().is_in_group("foods") and body.name == respie_layers[Respie_lvl]:
 			body.queue_free()
 			layers_bowl.frame += 1
 			Respie_lvl += 1
 			
-			
-			print(Respie_making)
 
 
 func _on_area_sause_area_entered(area):
+	var body = area.get_parent()
 	if Respie_making == 2:
-		print("hello")
-		layers_bowl.visible = false
 		$SauceBowl.visible = true
-		var body = area.get_parent()
-		if area.get_parent().is_in_group("foods"):
-			body.queue_free()
-			$SauceBowl.frame += 1
-			print(body.name)
+		if area.get_parent().is_in_group("foods") and body.name == respie_sauce[Respie_lvl]:
+			if area.get_parent().is_in_group("foods"):
+				body.queue_free()
+				$SauceBowl.frame += 1
+				Respie_lvl += 1
+			
 
 
 
